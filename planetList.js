d6ebdb676304b4c0665e0cde8c5397540af18ed8
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-03-02 10:23:13 +0100

    add fp article layout

--- planetList.js
+++ planetList.js
@@ -8,9 +8,10 @@ function planetList(planets){
     let output = `
         <article class="article planetListArticle" onclick="window.location.href='/planet.html?sphere=${planet.name}'" >
             <h1 class="planetListheadline1">${planet.name}</h1>
-            <p class="planetListWelcomeP">Welcome to the ${planet.climate} planet of ${planet.name}.</p> 
-            <p class="planetListLandscapeP">Here, the landscape is dominated by ${planet.terrain} and such.</p>
-
+            <div class="flexDiv">
+                <p class="planetListWelcomeP">Welcome to the ${planet.climate} planet of ${planet.name}.</p> 
+                <p class="planetListLandscapeP">Here, the landscape is dominated by ${planet.terrain} and such.</p>
+            </div>
         </article>
 
     
